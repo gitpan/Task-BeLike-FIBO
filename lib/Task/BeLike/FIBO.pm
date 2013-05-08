46a4e046dd37a1b51fc27d3a70689b98a3209b12
Gianluca Casati <fibo@cpan.org>
2013-05-08 10:00:18 +0000

    Import of FIBO/Task-BeLike-FIBO-20130508 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-FIBO
    gitpan-cpan-version:      20130508
    gitpan-cpan-path:         FIBO/Task-BeLike-FIBO-20130508.tar.gz
    gitpan-cpan-author:       FIBO
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/FIBO.pm
+++ lib/Task/BeLike/FIBO.pm
@@ -2,9 +2,121 @@ use strict;
 use warnings;
 package Task::BeLike::FIBO;
 {
-  $Task::BeLike::FIBO::VERSION = '0.004';
+  $Task::BeLike::FIBO::VERSION = '20130508';
 }
 # ABSTRACT: Leonardo Pisano a.k.a. Fibonacci
 
 1;
+
+=head1 STUFF INCLUDED
+
+=over 4
+
+=item *
+
+L<App::cpanminus> 1.5019
+
+=item *
+
+L<boolean> 0.28
+
+=item *
+
+L<CPAN> 2.00
+
+=item *
+
+L<DBI> 1.623
+
+=item *
+
+L<DBIx::Class> 0.08200
+
+=item *
+
+L<DBIx::Class::Schema::Loader> 0.07033
+
+=item *
+
+L<Dist::Zilla> 4.300028
+
+=item *
+
+L<Dist::Zilla::MintingProfile::Author::FIBO> 20130507
+
+=item *
+
+L<File::HomeDir> 1.00
+
+=item *
+
+L<List::Util> 1.25
+
+=item *
+
+L<List::MoreUtils> 0.33
+
+=item *
+
+L<Mojolicious> 3.70
+
+=item *
+
+L<Moose> 2.0604
+
+=item *
+
+L<MooseX::App> 1.09
+
+=item *
+
+L<MooseX::ClassAttribute> 0.26
+
+=item *
+
+L<MooseX::Types::Path::Class> 0.06
+
+=item *
+
+L<MooseX::Types::Structured> 0.28
+
+=item *
+
+L<Perl::Tidy> 20121207
+
+=item *
+
+L<Perl::Critic> 1.118
+
+=item *
+
+L<Perl::Critic::Bangs> 1.10
+
+=item *
+
+L<Path::Class> 0.29
+
+=item *
+
+L<Scalar::Util> 1.25
+
+=item *
+
+L<Test::More> 0.98
+
+=item *
+
+L<Test::use::ok> 0.11
+
+=item *
+
+L<Throwable::Error> 0.102080
+
+=item *
+
+L<YAML> 0.84
+
+=back
+
+=cut
 
